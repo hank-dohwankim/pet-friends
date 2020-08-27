961105a77b5520964945933f219d227298795612
hank.dohwankim <hank.dohwankim@gmail.com>
2020-08-26 22:48:31 -0500

    user post api done

---
+++ routes/api/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// @route  POST api/user
+// @desc   Register user
+// @access Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more caracters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User Route');
+  }
+);
+
+module.exports = router;
