9c3f7d1e8b68e4d5e104ca51b9fee28d537062c2
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-25 00:01:49 -0500

    dataFromSnapshot added in firestoreService.js

--- src/features/meetings/meetingDashboard/MeetingDashboard.jsx
+++ src/features/meetings/meetingDashboard/MeetingDashboard.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
-import { getMeetingFromFireStore } from './../../../app/firestore/firestoreService';
+import {
+  getMeetingFromFirestore,
+  dataFromSnapshot,
+} from './../../../app/firestore/firestoreService';
 
 export default function MeetingDashboard() {
   // Ignite once component amount
   useEffect(() => {
-    const unsubscribe = getMeetingFromFireStore({
+    const unsubscribe = getMeetingFromFirestore({
       next: (snapshot) =>
-        console.log(snapshot.docs.map((docSnapshot) => docSnapshot.data())),
+        console.log(
+          snapshot.docs.map((docSnapshot) => dataFromSnapshot(docSnapshot))
+        ),
       error: (error) => console.log(error),
     });
     return unsubscribe;
