c2f010295ce42c829602f13770a9cd1bde046745
Han kyung Sung <hankyungsung@Hans-MBP.hitronhub.home>
2020-10-07 00:49:27 -0500

    Added a router and basic structure for navigation bar.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import MeetingDashboard from '../../features/meetings/meetingDashboard/MeetingDashboard';
+import NavigationBar from "../../features/nav/NavigationBar";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import LoginForm from "../../features/auth/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <>Pet Friends</>
+      <NavigationBar />
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </Router>
       <MeetingDashboard />
     </div>
   );
