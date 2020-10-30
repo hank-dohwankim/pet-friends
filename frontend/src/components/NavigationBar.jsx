37fba6352391339cf6399cbd2a11a348b272e925
hank.dohwankim <hank.dohwankim@gmail.com>
2020-10-29 23:57:53 -0500

    nav bar toggle btn added

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand>Pet Friend</Navbar.Brand>
-      </LinkContainer>
-      <LinkContainer to="/login">
-        <Nav.Link>
-          <p>Login</p>
-        </Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/register">
-        <Nav.Link>
-          <p>Sign up</p>
-        </Nav.Link>
-      </LinkContainer>
-    </Navbar>
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Pet Friend</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link>Sign up</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
